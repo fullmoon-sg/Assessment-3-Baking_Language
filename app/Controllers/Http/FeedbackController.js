40b08d54c7566fab9384be1917b47dfa438c0411
fullmoon-sg <mbfoo7757@gmail.com>
2021-01-27 04:04:13 +0000

    Able to view feedback

--- app/Controllers/Http/FeedbackController.js
+++ app/Controllers/Http/FeedbackController.js
@@ -8,9 +8,10 @@ const Config = use('Config')
 class FeedbackController {
 
   async index({view}){
-
+    let products = await Product.all();
     let feedbacks = await Feedback.all();
     return view.render('feedbacks/index',{
+      products : products.toJSON(),
       feedbacks : feedbacks.toJSON()
     })
   }
@@ -23,13 +24,12 @@ class FeedbackController {
   let body = request.post();
   let product = await Product.find(params.product_id);
    let feedback = new Feedback();
-   console.log(body)
-   console.log(product)
    feedback.category = body.category;
    feedback.comments = body.comments;
    feedback.ratings = body.ratings;
+   feedback.image = product.image_url
    feedback.customer_id = auth.user.customer_id;
-  feedback.product_id = product.id;
+   feedback.product_id = product.id;
    await feedback.save();
    return response.redirect('/feedbacks')
  }
--- database/migrations/1611652752722_feedback_schema.js
+++ database/migrations/1611652752722_feedback_schema.js
@@ -8,9 +8,9 @@ class FeedbackSchema extends Schema {
     this.create('feedbacks', (table) => {
       table.increments()
       table.string('category',100).notNullable();
-      table.string('comments',500).notNullable();
+      table.string('comments',2500).notNullable();
       table.integer('ratings').notNullable();
-
+      table.string('image',100).notNullable();
       table.integer('customer_id').unsigned().notNullable();
       table.foreign('customer_id').references('customers.id');
 
